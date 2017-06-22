60d7598e9ad5317bfe7dac34071e4b2f9323b4af
Philip White <philipwhite@cedarville.edu>
2017-06-22 10:15:39 -0400

    Remove old dependencies on redis and postgres

--- shell.nix
+++ shell.nix
@@ -12,38 +12,12 @@ pycharmOrcle = idea.pycharm-community.override {
   jdk = oraclejdk8;
 };
 
-requests-oauth = pkgs.python35Packages.buildPythonPackage rec {
-  name = "requests-oauthlib";
-
-  src = fetchurl {
-    url = "http://github.com/requests/requests-oauthlib/archive/v0.8.0.tar.gz";
-    sha256 = "18gg9dwral153c10f8bwhz2dy4nw7c6mws5a2g7gidk3z5xhqy4n";
-  };
-
-  doCheck = false;        # Internet tests fail when building in chroot
-  propagatedBuildInputs = with pkgs.python35Packages; [ oauthlib requests ];
-};
-
-python-twitter = pkgs.python35Packages.buildPythonPackage {
-  name = "python-twitter";
-
-  src = fetchurl {
-    url = "https://github.com/bear/python-twitter/archive/v3.3.tar.gz";
-    sha256 = "18ychdm4wd6wa8jb97gvsqcfbnx0gfvk5hvhknld34qfpfmgjkxk";
-  };
-  
-  doCheck = false;
-  propagatedBuildInputs = with pkgs.python35Packages; [ 
-    future requests requests-oauth ];
-
-};
-
 in
 pkgs.python35Packages.buildPythonPackage rec {
   name = "reu-research";
   buildInputs = [ 
-    vim sqlite sqlitebrowser redis pycharmOrcle python35 graphviz inkscape ];
+    vim sqlite sqlitebrowser pycharmOrcle python35 graphviz ];
   propagatedBuildInputs = with pkgs.python35Packages; [ 
     pytest pyqt4 requests networkx 
-    matplotlib numpy pandas graphviz python-twitter ];
+    matplotlib numpy pandas graphviz ];
 }
