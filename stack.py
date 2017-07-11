ed8fd7c323abf8f7863eecb7c78930e1e7d2658d
Philip White <philipwhite@cedarville.edu>
2017-07-11 13:22:16 -0400

    Add stack reloading code

--- stack.py
+++ stack.py
@@ -16,7 +16,7 @@ def xml_children(filename):
 User = namedtuple('User', 'id display_name reputation website_url age location')
 Tag = namedtuple('Tag', 'id name count')
 Comment = namedtuple('Comment', 'id post_id score user_id created_at')
-Post = namedtuple('Post', 'id post_type_id score owner_user_id accepted_answer_id created_at tags')
+Post = namedtuple('Post', 'id score owner_user_id parent_id created_at tags')
 
 def parse_date(date_str):
     return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
@@ -46,47 +46,67 @@ def comment_from_xml(xml):
                 id=xml['Id'],
                 post_id=xml['PostId'],
                 score=xml['Score'],
-                user_id=xml['UserId'])
+                user_id=xml['UserId'],
+                created_at=parse_date(xml['CreationDate']))
 
-def store_comment(comment, conn):
-    conn.execute('INSERT INTO StackComments VALUES(?,?,?,?)', comment)
+def comment_from_db(row):
+    (c_id, post_id, score, user_id, created_at_stamp) = row
+    return Comment(
+            id=c_id,
+            post_id=post_id,
+            score=score,
+            user_id=user_id,
+            created_at=datetime.utcfromtimestamp(created_at_stamp))
+
+def store_comment(c, conn):
+    vs = (c.id, c.post_id, c.score, c.user_id, c.created_at.timestamp())
+    conn.execute('INSERT INTO StackComments VALUES(?,?,?,?,?)', vs)
 
 def fetch_comment_id(comment_id, conn):
     row = conn.execute(
             'SELECT * FROM StackComments WHERE Id = ?', 
             (comment_id,)).fetchone()
-    return Comment(*row)
+    return comment_from_db(row)
+    
 
-def post_from_xml(xml, tag_data):
-    tags_str = child.get('Tags', None)
-    tags=[]
-    if tags_str:
-        tag_names = re.findall('<(.*?)>', tags_str)
-        tags = {tag_data[name] for name in tag_names}
+def post_from_xml(xml):
+    tags = {}
+    if 'Tags' in xml:
+        tags = set(re.findall('<(.*?)>', xml['Tags']))
+    if 'OwnerUserId' in xml:
+        return Post(
+                id=xml['Id'],
+                score=xml['Score'],
+                owner_user_id=xml['OwnerUserId'],
+                parent_id= xml.get('ParentId', None),
+                created_at=parse_date(xml['CreationDate']),
+                tags=tags)
+
+def post_from_db(row, conn):
+    (p_id, score, owner_id, parent_id, created_at_stamp) = row
+    tags = [tag for (tag,) in conn.execute(
+        'SELECT Tag FROM StackTagAssignments WHERE PostId = ?',
+        (p_id,))]
     return Post(
-            id=xml['Id'],
-            post_type_id=xml['PostTypeId'],
-            score=xml['Score'],
-            owner_user_id=xml['OwnerUserId'],
-            accepted_answer_id=xml['AcceptedAnswerId'],
-            created_at=parse_date(xml['CreationDate']),
-            tag_ids=tags)
+            id=p_id,
+            score=score,
+            owner_user_id=owner_id,
+            parent_id=parent_id,
+            created_at=datetime.utcfromtimestamp(created_at_stamp),
+            tags=tags)
 
-def store_post(post, conn):
-    values = (post.id, post.post_type_id, post.score, 
-            post.owner_user_id, post.accepted_answer_id,
-            post.created_at.timestamp())
-    conn.execute('INSERT INTO StackPosts VALUES(?,?,?,?,?,?,?)', values)
-    for tag in post.tags:
+def store_post(p, conn):
+    values = (p.id, p.score, p.owner_user_id, p.parent_id, p.created_at.timestamp())
+    conn.execute('INSERT INTO StackPosts VALUES(?,?,?,?,?)', values)
+    for tag in p.tags:
         conn.execute('INSERT INTO StackTagAssignments VALUES(?,?)',
-                    (post.id, tag.id))
+                    (p.id, tag))
 
 def fetch_post_id(post_id, conn):
-    (p_id, post_type_id, score, owner_id, 
-            accepted_id, created_at_stamp) = conn.execute(
-                    'SELECT * FROM StackPosts WHERE Id = ?',
-                    (post_id,)).fetchone()
-
+    row = conn.execute(
+            'SELECT * FROM StackPosts WHERE Id = ?',
+            (post_id,)).fetchone()
+    return post_from_db(row, conn)
 
 def reload_tags(conn):
     print('Deleting tag data')
@@ -104,7 +124,6 @@ def reload_users(conn):
         if user is not None:
             store_user(user, conn)
         if count % 10000 == 0: 
-            conn.commit()
             print(count, 'lines processed')
 
 def reload_comments(conn):
@@ -116,23 +135,18 @@ def reload_comments(conn):
         if comment is not None:
             store_comment(comment, conn)
         if count % 100000 == 0: 
-            conn.commit()
             print(count / 1000000.0)
 
 def reload_posts(conn):
     print('Deleting post data')
     conn.execute('DELETE FROM StackPosts')
     children = xml_children('data/Posts.xml')
-    tag_rows = conn.execute(
-        'SELECT * FROM StackTags').fetchall()
-    tag_data = {name: Tag(id=id, name=name, count=count) 
-            for (id, name, count) in tag_rows}
     print('Loading post data')
     for count, child in enumerate(children):
-        post = post_from_xml(child, tag_data)
-        store_post(post, conn)
+        post = post_from_xml(child)
+        if post is not None:
+            store_post(post, conn)
         if count % 10000 == 0: 
-            conn.commit()
             print(count / 1000000.0)
 
 def user_to_json(user):
@@ -143,17 +157,33 @@ def user_to_json(user):
             'age': user.age,
             'location': user.location}
 
-def fetch_id(user_id, conn):
+def user_fetch_id(user_id, conn):
     row = conn.execute(
             'SELECT * FROM StackUsers WHERE Id = ?',
             (user_id,)).fetchone()
     return User(*row)
 
+def user_fetch_display_name(display_name, conn):
+    row = conn.execute(
+            'SELECT * FROM StackUsers WHERE DisplayName = ?',
+            (display_name,)).fetchone()
+    return User(*row)
+
+def user_posts(user, conn):
+    return [post_from_db(row, conn) for row in conn.execute(
+        'SELECT * FROM StackPosts WHERE OwnerUserId = ?',
+        (user.id,))]
+
+def user_comments(user, conn):
+    return [comment_from_db(row) for row in conn.execute(
+        'SELECT * FROM StackComments WHERE UserId = ?',
+        (user.id,))]
+
 def user_answerers(user, conn):
     rows = conn.execute('''
         SELECT answerers.* FROM StackUsers su
         JOIN StackPosts sp ON su.Id = sp.OwnerUserId
-        JOIN StackPosts answers ON answers.Id = sp.AcceptedAnswerId
+        JOIN StackPosts answers ON answers.ParentId = sp.Id
         JOIN StackUsers answerers ON answerers.Id = answers.OwnerUserId
         WHERE su.Id = ?''', (user.id,)).fetchall()
     return [User(*row) for row in rows]
@@ -166,3 +196,6 @@ def user_questioners(user, conn):
         JOIN StackUsers questioners ON questioners.Id = questions.OwnerUserId
         WHERE su.Id = ?''', (user.id,)).fetchall()
     return [User(*row) for row in rows]
+
+def user_degree(user, conn):
+    return len(user_answerers(user, conn)) + len(user_questioners(user, conn))
