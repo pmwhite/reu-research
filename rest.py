27b0793f364787dc7442cf5c57f6a6d16474a497
Philip White <pmw1197@gmail.com>
2017-07-14 16:22:53 -0400

    Make rest calls stubborn and cached

---
+++ rest.py
@@ -0,0 +1,36 @@
+import pickle
+import shelve
+import requests
+import time
+
+shelf = shelve.open('data/shelf')
+
+def cached_get(path, params=None, **kwargs):
+    h = str(b'get' + pickle.dumps(path) + pickle.dumps(params) + pickle.dumps(kwargs))
+    if h in shelf:
+        return (True, shelf[h])
+    else:
+        response = None
+        while response == None:
+            try:
+                response = requests.get(path, params=params, **kwargs)
+            except Exception as e:
+                print('didn\'t get response, trying again for you in 3 seconds')
+                time.sleep(3)
+        shelf[h] = response
+        return (False, response)
+
+def cached_post(url, data=None, json=None, **kwargs):
+    h = str(b'post' + pickle.dumps(url) + pickle.dumps(data) + pickle.dumps(json) + pickle.dumps(kwargs))
+    if h in shelf:
+        return (True, shelf[h])
+    else:
+        response = None
+        while response == None:
+            try:
+                response = requests.post(url, data=data, json=json, **kwargs)
+            except Exception as e:
+                print('didn\'t get response, trying again for you in 3 seconds')
+                time.sleep(3)
+        shelf[h] = response
+        return (False, response)
